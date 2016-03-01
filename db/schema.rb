1e27aaf3be5140ff62e721c5e6372c773245360a
HDking <h.c.dinh1@gmail.com>
2016-03-01 15:10:12 +0000

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109211104) do
+ActiveRecord::Schema.define(version: 20160301144556) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["name"], name: "index_users_on_name", unique: true
